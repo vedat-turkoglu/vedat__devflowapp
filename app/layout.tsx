7919d0f6306bc597845587b0582536a493ed58a5
vedat <vedat.devs@gmail.com>
2024-11-18 19:45:34 +0300

    assets & meta data added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,25 @@ import localFont from "next/font/local";
 import React from "react";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const inter = localFont({
+  src: "./fonts/interVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const spaceGrotesk = localFont({
+  src: "./fonts/spaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700",
 });
 
 export const metadata: Metadata = {
   title: "devflow",
-  description: "like stack overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 const Home = () => {
   return (
-    <h1 className="text-center text-3xl font-black text-violet-700">
-      This is Home Page :)
-    </h1>
+    <>
+      <h1 className="text-center text-3xl font-black text-violet-700">
+        This is Home Pagey :)
+      </h1>
+      <h1 className="text-center font-space-grotesk text-3xl  font-black">
+        This is Home spaceGrotesk family
+      </h1>
+    </>
   );
 };
 
