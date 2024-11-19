ee71f6845963ae01a89ff6e4d81bc89882c5fcf7
vedat <vedat.devs@gmail.com>
2024-11-19 16:11:18 +0300

    set up light & dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ const Home = () => {
       <h1 className="text-center text-3xl font-black text-violet-700">
         This is Home Pagey :)
       </h1>
-      <h1 className="text-center font-space-grotesk text-3xl  font-black">
-        This is Home spaceGrotesk family
-      </h1>
     </>
   );
 };
